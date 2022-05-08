698e7c0396eb5bc044f28d5a5dc6869488295ea3
Mikhail Doshevsky <m.doshevsky@gmail.com>
2022-05-08 17:18:48 +0300

    Implement app layout

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
     selector: 'app-root',
     templateUrl: './app.component.html',
     styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-    title = 'tag-files';
+export class AppComponent implements OnInit {
+    public nodes: string[] = [];
+
+    public ngOnInit(): void {
+        for (let i = 0; i < 54; ++i) {
+            this.nodes.push(`file${i}.txt`);
+        }
+    }
 }
