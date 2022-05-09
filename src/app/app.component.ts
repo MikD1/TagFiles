35838c878c63d7d8bf45df0cb3093f2cb5c9256d
Mikhail Doshevsky <m.doshevsky@gmail.com>
2022-05-09 14:12:57 +0300

    Top panel layout

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,9 +6,12 @@ import { Component, OnInit } from '@angular/core';
     styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit {
+    public locationParts: string[] = [];
     public nodes: string[] = [];
 
     public ngOnInit(): void {
+        this.locationParts = ['Users', 'mik', 'Downloads', 'test'];
+
         for (let i = 0; i < 54; ++i) {
             this.nodes.push(`file${i}.txt`);
         }
